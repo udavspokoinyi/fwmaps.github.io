a371ddd0016ab805892de90c15d2fcb1c38d3637
udavspokoinyi <60442562+udavspokoinyi@users.noreply.github.com>
2020-01-29 22:36:26 +0500

    1 commit
    summer

---
+++ FWmaps/main.js
@@ -0,0 +1,392 @@
+
+$('#sun').ready(function(){
+	$('#sun').hide();
+	
+	
+	
+	
+	
+});
+$('#iopd').ready(function(){
+	$('#iopd').hide();
+	
+	
+	
+	
+});
+$('#uhg').click(function(){
+	
+      $('#iopd').slideFadeToggle('slow');
+
+    
+	
+	
+	
+	
+});
+
+$('#iopg').ready(function(){
+	$('#iopg').hide();
+	
+	
+	
+	
+});
+$('#uhg').click(function(){
+	
+      $('#inst').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbk').click(function(){
+	
+      $('#inst').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbl').click(function(){
+	
+      $('#inst').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbl').click(function(){
+	
+      $('#iopg').slideFadeToggle('slow');
+
+    
+	
+	
+	
+	
+});
+$('#uhg').click(function(){
+	
+      $('#grow').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#uhg').click(function(){
+	
+      $('#backnews').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbl').click(function(){
+	
+      $('#backnews').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbk').click(function(){
+	
+      $('#backnews').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#newsshow').click(function(){
+	
+      $('#grow').slideDown('slow');
+
+    
+	
+	
+	
+	
+});
+$('#newsshow').click(function(){
+	
+      $('#backnews').slideDown('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbk').click(function(){
+	
+      $('#grow').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#jbl').click(function(){
+	
+      $('#grow').slideUp('slow');
+
+    
+	
+	
+	
+	
+});
+$('#iopf').ready(function(){
+	$('#iopf').hide();
+	
+	
+	
+	
+});
+$('#jbk').click(function(){
+	
+      $('#iopf').slideFadeToggle('slow');
+
+    
+	
+	
+	
+	
+});
+
+
+
+$('#sex').ready(function(){
+	$('#sex').hide();
+	
+	
+	
+	
+	
+});
+	
+	
+	
+	
+$('#kun').ready(function(){
+	$('#kun').hide();
+	
+	
+	
+	
+	
+});
+$('#bla').ready(function(){
+	$('#bla').hide();
+	
+	
+	
+	
+	
+});
+
+
+$('#bgh').ready(function(){
+	
+	$('#bgh').hide();
+	$('#bgh').slideDown('slow');
+	
+	
+	
+});
+$('#kef').ready(function(){
+	
+	$('#kef').hide();
+	$('#kef').slideDown('slow');
+	
+	
+	
+});
+$('#nfl').ready(function(){
+	
+      $('#nfl').fadeOut('slow');
+    
+	
+	
+	
+	
+});
+$('#nfp').ready(function(){
+	
+     $('#nfp').fadeOut('slow');
+    
+	
+	
+	
+	
+});
+$('#nft').ready(function(){
+	
+      $('#nft').fadeOut('slow');
+    
+	
+	
+	
+	
+});
+$('#nfg').ready(function(){
+	
+      $('#nfg').fadeOut('slow');
+    
+	
+	
+	
+	
+});
+
+   $.fn.slideFadeToggle = function(speed, easing, callback){
+      return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
+    };
+
+    $('#mex').click(function(){
+      $('#nfl').slideFadeToggle('slow');
+      $('#nfp').slideFadeToggle('slow');
+      $('#nft').slideFadeToggle('slow');
+      $('#nfg').slideFadeToggle('slow');	
+    });
+$('#mex').click(function(){
+	
+      $('#mex').slideUp();
+    
+	
+	
+	
+	
+});
+   $.fn.slideFadeToggle = function(speed, easing, callback){
+      return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
+    };
+
+    $('#tuk').click(function(){
+      $('#sex').slideFadeToggle('slow');
+      $('#kun').slideFadeToggle('slow');
+      $('#sun').slideFadeToggle('slow');
+      $('#bla').slideFadeToggle('slow');	
+    });
+
+
+$('#g6').ready(function(){
+
+	$('#g6').slideUp('slow')
+	$('#g6').slideDown('slow')
+
+
+});
+$('#sjb').ready(function(){
+
+	$('#sjb').fadeOut();
+	$('#sjb').slideDown('slow');
+
+
+
+});
+$('#kis').ready(function(){
+
+	$('#kis').slideUp('slow')
+	
+
+
+});
+
+
+$('.gril').ready(function(){
+	
+     $('.gril').hide();
+});
+$('.gril').ready(function(){
+	
+     $('.gril').slideFadeToggle('slow');
+});
+$('#beta').ready(function(){
+ 	$('#beta').slideUp('fast');
+
+
+
+
+
+});
+$('#beta').ready(function(){
+ 	$('#beta').slideDown('slow');
+
+
+
+
+
+});
+
+var cssSelector = anime({
+	delay:300,
+	targets: '#fis',
+	translateY: 300,
+	loop: true,
+	direction:'alternate',
+	duration: 3000,
+
+	
+});
+var cssSelector = anime({
+	delay:600,
+	targets: '#sis',
+	translateY: 300,
+	loop: true,
+	direction:'alternate',
+	duration: 3000,
+
+});
+var cssSelector = anime({
+	delay:100,
+	targets: '#zis',
+	translateY: 300,
+	loop: true,
+	direction:'alternate',
+	duration: 3000,
+
+});
+var cssSelector = anime({
+	delay:200,
+	targets: '#bis',
+	translateY: 300,
+	loop: true,
+	direction:'alternate',
+	duration: 3000,
+
+});
+var cssSelector = anime({
+	delay:400,
+	targets: '#qis',
+	translateY: 300,
+	loop: true,
+	direction:'alternate',
+	duration: 3000,
+
+});
+var cssSelector = anime({
+	delay:700,
+	targets: '#xis',
+	translateY: 300,
+	loop: true,
+	direction:'alternate',
+	duration: 3000,
+
+});
